42ba7f58ec2cd7f05e2c823fcf31f9b945cb048a
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2023-07-19 23:26:02 +0300

    yeni

---
+++ php/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>The Cappa Luxury Hotel</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="stylesheet" href="/css/css2?family=Barlow&family=Barlow+Condensed&family=Gilda+Display&display=swap">
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
---
+++ php/header.php
@@ -0,0 +1,97 @@
+<body>
+    <!-- Preloader -->
+    <div class="preloader-bg"></div>
+    <div id="preloader">
+        <div id="preloader-status">
+            <div class="preloader-position loader"> <span></span> </div>
+        </div>
+    </div>
+    <!-- Progress scroll totop -->
+    <div class="progress-wrap cursor-pointer">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewbox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
+        </svg>
+    </div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <!-- Logo -->
+            <div class="logo-wrapper">
+                <a class="logo" href="index.php"> <img src="img/logo.png" class="logo-img" alt=""> </a>
+                <!-- <a class="logo" href="index.php"> <h2>THE CAPPA <span>Luxury Hotel</span></h2> </a> -->
+            </div>
+            <!-- Button -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"><i class="ti-menu"></i></span> </button>
+            <!-- Menu -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Anasayfa</a></li>
+
+                    <!-- <li class="nav-item dropdown"> <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Anasayfa <i class="ti-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="index.php" class="dropdown-item active"><span>Home Layout 1</span></a></li>
+                            <li><a href="index2.html" class="dropdown-item"><span>Home Layout 2</span></a></li>
+                            <li><a href="index3.html" class="dropdown-item"><span>Home Layout 3</span></a></li>
+                            <li><a href="index4.html" class="dropdown-item"><span>Home Layout 4</span></a></li>
+                            <li><a href="index5.html" class="dropdown-item"><span>Video 1</span></a></li>
+                            <li><a href="index6.html" class="dropdown-item"><span>Video 2</span></a></li>
+                            <li><a href="index7.html" class="dropdown-item"><span>Video 3</span></a></li>
+                            <li><a href="index8.html" class="dropdown-item"><span>Slideshow 1</span></a></li>
+                            <li><a href="index9.html" class="dropdown-item"><span>Slideshow 2</span></a></li>
+                            <li><a href="index10.html" class="dropdown-item"><span>Slideshow 3</span></a></li>
+                            <li><a href="index11.html" class="dropdown-item"><span>Parallax Image 1</span></a></li>
+                            <li><a href="index12.html" class="dropdown-item"><span>Parallax Image 2</span></a></li>
+                            <li><a href="index13.html" class="dropdown-item"><span>Parallax Image 3</span></a></li>
+                            <li><a href="index14.html" class="dropdown-item"><span>Grid Background 1</span></a></li>
+                            <li><a href="index15.html" class="dropdown-item"><span>Grid Background 2</span></a></li>
+                        </ul>     
+                    </li> -->
+                    <li class="nav-item"><a class="nav-link" href="about.html">Hakkımızda</a></li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">ODALAR & SUİTLER <i class="ti-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="room-details.html" class="dropdown-item"><span>Oda Tipi</span></a></li>
+                            <!-- <li><a href="rooms2.html" class="dropdown-item"><span>Rooms 2</span></a></li>
+                            <li><a href="rooms3.html" class="dropdown-item"><span>Rooms 3</span></a></li>
+                            <li><a href="room-details.html" class="dropdown-item"><span>Room Details</span></a></li> -->
+                        </ul>
+                    </li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="restaurant.html">Restaurant</a></li>
+                    <li class="nav-item"><a class="nav-link" href="spa-wellness.html">Spa</a></li> -->
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Pages <i class="ti-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="services.html" class="dropdown-item"><span>Services</span></a></li>
+                            <li><a href="facilities.html" class="dropdown-item"><span>Facilities</span></a></li>
+                            <li><a href="gallery.html" class="dropdown-item"><span>Gallery</span></a></li>
+                            <li><a href="team.html" class="dropdown-item"><span>Team</span></a></li>
+                            <li><a href="pricing.html" class="dropdown-item"><span>Pricing</span></a></li>
+                            <li><a href="careers.html" class="dropdown-item"><span>Careers</span></a></li>
+                            <li><a href="faq.html" class="dropdown-item"><span>F.A.Qs</span></a></li>
+                            <li class="dropdown-submenu dropdown"> <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" href="#"><span>Other Pages <i class="ti-angle-right"></i></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="404.html" class="dropdown-item"><span>404 Page</span></a></li>
+                                    <li><a href="coming-soon.html" class="dropdown-item"><span>Coming Soon</span></a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">AKTİVİTELER<i class="ti-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="news.html" class="dropdown-item"><span>News 1</span></a></li>
+                            <li><a href="news2.html" class="dropdown-item"><span>News 2</span></a></li>
+                            <li><a href="post.html" class="dropdown-item"><span>Post Page</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">News <i class="ti-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="news.html" class="dropdown-item"><span>News 1</span></a></li>
+                            <li><a href="news2.html" class="dropdown-item"><span>News 2</span></a></li>
+                            <li><a href="post.html" class="dropdown-item"><span>Post Page</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
